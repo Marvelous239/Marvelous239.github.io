c1a5d0f8886cae7917bcf659f0c2127e4a3f3fe7
Marvelous239 <marvelous239@gmail.com>
2024-02-22 20:46:23 -0500

    Add editor

---
+++ editor/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"syscall/js"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+
+	"fyne.io/fyne/v2/widget"
+)
+
+var MainEntry = widget.NewMultiLineEntry()
+
+func loadFile() {
+	input := js.Global().Get("document").Call("createElement", "input")
+	input.Set("type", "file")
+	input.Call("addEventListener", "change", js.FuncOf(readFile), "false")
+	input.Call("click")
+}
+
+func readFile(this js.Value, args []js.Value) any {
+	file := this.Get("files").Index(0)
+	if file.IsNull() {
+		return nil
+	}
+
+	reader := js.Global().Get("FileReader").New()
+	reader.Set("onload", js.FuncOf(func(this js.Value, args []js.Value) any {
+		MainEntry.SetText(this.Get("result").String())
+		MainEntry.Refresh()
+		return nil
+	}))
+	reader.Call("readAsText", file)
+
+	return nil
+}
+
+func saveFile() {
+	encodeURIComponent := js.Global().Get("encodeURIComponent")
+
+	link := js.Global().Get("document").Call("createElement", "a")
+	data := "data:text/plain;charset=utf-8," + encodeURIComponent.Invoke(MainEntry.Text).String()
+	link.Set("href", data)
+	link.Set("download", "subs.txt")
+
+	MainEntry.SetText("")
+	MainEntry.Refresh()
+
+	link.Call("click")
+}
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Fyne App")
+
+	toolbar := widget.NewToolbar(
+		widget.NewToolbarAction(theme.FileIcon(), loadFile),
+		widget.NewToolbarAction(theme.DocumentSaveIcon(), saveFile),
+	)
+
+	MainEntry.TextStyle.Monospace = true
+	scrollContainer := container.NewScroll(MainEntry)
+	scrollContainer.SetMinSize(fyne.NewSize(200, 400))
+
+	content := container.NewVBox(
+		layout.NewSpacer(),
+		toolbar,
+		scrollContainer,
+		layout.NewSpacer(),
+	)
+
+	myWindow.SetContent(content)
+	myWindow.ShowAndRun()
+}
